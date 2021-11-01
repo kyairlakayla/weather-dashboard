77a1b033442670933746c0e7e23aa6eb958fbb81
Kayla <kyairlakayla.davis@gmail.com>
2021-10-31 21:39:15 -0400

    finish find current weather function

--- assets/script.js
+++ assets/script.js
@@ -8,47 +8,42 @@ var getCurrentWeather = (event) => {
     currentCity = $('#search-city').val();
 
     // fetch queryUrl from API 
-    let queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + appKey;
+    let queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city +"&units=imperial&appid=" + appKey;
     fetch(queryUrl)
-//    .then(catchErrors)
     .then((response) => {
         return response.json();
     })
     .then((response) => {
-    //    getFiveDayForecast(event);
-
         // create html for current city 
         let currentWeatherHTML = `
         <h3>${response.name} 
         <ul class="list-unstyled">
-            <li>Tempature: ${response.main.temp}</li>
+            <li>Tempature: ${response.main.temp}&#8457;</li>
             <li>Humidty: ${response.main.humidity}%</li>
             <li>Wind Speed: ${response.wind.speed} mph</li>
             <li id="uvIndex">UV Index:</li>
         </ul>`;
-        $('current-weather').html(currentWeatherHTML);
+        $('#current-weather').html(currentWeatherHTML);
         // fetch UV index from API 
-//        let latitude = response.coord.lat;
-//        let longitude = response.coord.lon;
-//        let uvQueryURL = "api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&APPID=" + appKey;
+        let latitude = response.coord.lat;
+        let longitude = response.coord.lon;
+        let uvQueryURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&appid=" + appKey;
 
-
-//        fetch(uvQueryURL)
-//        .then(handleErrors)
-//        .then((response) => {
-//            return response.json();
-//        })
-//        .then((response) => {
-//            let uvIndex = response.value;
-//            $('#uvIndex').html(`UV Index: <span id="uvVal"> ${uvIndex}</span>`);
-//            if (uvIndex>=0 && uvIndex<3){
-//                $('#uvVal').attr("class", "uv-favorable");
-//            } else if (uvIndex>=3 && uvIndex<8){
-//                $('#uvVal').attr("class", "uv-moderate");
-//            } else if (uvIndex>=8){
-//                $('#uvVal').attr("class", "uv-severe");
-//            }
-//        });
+        fetch(uvQueryURL)
+        .then((response) => {
+            return response.json();
+        })
+        .then((response) => {
+            let uvIndex = response.value;
+            $('#uvIndex').html(`UV Index: <span id="uvVal"> ${uvIndex}</span>`);
+            if (uvIndex>=0 && uvIndex<3){
+                $('#uvVal').attr("class", "uv-favorable");
+            } else if (uvIndex>=3 && uvIndex<8){
+                $('#uvVal').attr("class", "uv-moderate");
+            } else if (uvIndex>=8){
+                $('#uvVal').attr("class", "uv-severe");
+            }
+        });
     })
 }
 
